45150f90cf9c7f29ece4dc2d948d5b137382d92c
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-05-15 00:06:41 +0200

    Commit

--- app/src/main/java/com/example/miravereda/activities/ContrasenyaActivity.java
+++ app/src/main/java/com/example/miravereda/activities/ContrasenyaActivity.java
@@ -5,7 +5,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import es.ieslavereda.miravereda.R;
+import es.ieslavereda.miravereda.R;;
 
 public class ContrasenyaActivity extends AppCompatActivity {
 
@@ -17,7 +17,6 @@ public class ContrasenyaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contrasenya);
 
         actualizarContrasenya = findViewById(R.id.bttnActualizarContrasenya);
-
         actualizarContrasenya.setOnClickListener(view -> finish());
     }
 
--- app/src/main/java/com/example/miravereda/activities/DetailsActivity.java
+++ app/src/main/java/com/example/miravereda/activities/DetailsActivity.java
@@ -1,17 +1,60 @@
 package com.example.miravereda.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import es.ieslavereda.miravereda.R;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private Button btnRestar;
+    private Button btnSumar;
+    private TextView valoracion;
+    private double auxiliar;
+    private FloatingActionButton guardarValoracion;
+    private Button adquirir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        btnRestar = findViewById(R.id.bttnMenos);
+        btnSumar = findViewById(R.id.bttnMas);
+        valoracion = findViewById(R.id.tvValoracionContenido);
+        auxiliar = 5;
+        guardarValoracion = findViewById(R.id.fabGuardar);
+        adquirir = findViewById(R.id.bttnAdquirir);
+
+        btnRestar.setOnClickListener(view -> {
+            if (auxiliar > 0) {
+                auxiliar -= 0.5;
+                valoracion.setText(String.valueOf(auxiliar));
+            }
+        });
+
+        btnSumar.setOnClickListener(view -> {
+            if (auxiliar < 10) {
+                auxiliar += 0.5;
+                valoracion.setText(String.valueOf(auxiliar));
+            }
+        });
+
+        guardarValoracion.setOnClickListener(view -> {
+            auxiliar = 5;
+            valoracion.setText(String.valueOf(auxiliar));
+            Toast.makeText(this, "Su valoración ha sido guardada correctamente", Toast.LENGTH_SHORT).show();
+        });
+
+        adquirir.setOnClickListener(view -> {
+            Toast.makeText(this, "Se ha añadido correctamente a la cesta", Toast.LENGTH_SHORT).show();
+            finish();
+        });
     }
 }
