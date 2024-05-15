0d8de5695c572efcc681149577b8e2d4e2180d58
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-05-15 15:31:29 +0200

    Commit

--- app/src/main/java/com/example/miravereda/base/Parameters.java
+++ app/src/main/java/com/example/miravereda/base/Parameters.java
@@ -3,12 +3,18 @@ package com.example.miravereda.base;
 public class Parameters {
 
     public final static String API = "<put_your_API_Key_here>";
+
+
+
     public final static String LANG = "es";
     public final static String UNITS = "metric";
-    public final static String URL = "https://api.openweathermap.org/data/2.5/";
     public final static String URL_OPTIONS =   "forecast?appid=" + API + "&lang=" + LANG + "&units=" + UNITS;
-
-    public final static String ICON_URL_PRE = "http://openweathermap.org/img/wn/";
     public static final String ICON_URL_POST = "@2x.png";
+
+
+
+
+    public final static String URL = "https://api.openweathermap.org/data/2.5/";
+    public final static String ICON_URL_PRE = "http://openweathermap.org/img/wn/";
 
 }
