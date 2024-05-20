ab057495f7ca438bc11ffd83d691aeb72ac3d8db
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-05-20 12:06:47 +0200

    Commit

--- app/src/main/java/com/example/miravereda/base/Parameters.java
+++ app/src/main/java/com/example/miravereda/base/Parameters.java
@@ -2,8 +2,8 @@ package com.example.miravereda.base;
 
 public class Parameters {
 
-    public final static String URL = "http://localhost:8080/miraveredaAPI/peliculas";
-    // "http://192.168.1.156:8080/miraveredaAPI/"
+    public final static String URL = "http://192.168.1.156:8080/miraveredaAPI/";
+    // "http://localhost:8080/miraveredaAPI/peliculas";
     public final static String ICON_URL_PRE = "http://openweathermap.org/img/wn/";
 
 }
